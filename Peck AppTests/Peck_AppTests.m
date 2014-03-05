91d90531dcfb1ca25c57d2daa1e4e2ddc73c71ef
ataylor0123 <ataylor0123@gmail.com>
2014-03-05 11:21:16 -0500

    Initial Commit

---
+++ Peck AppTests/Peck_AppTests.m
@@ -0,0 +1,34 @@
+//
+//  Peck_AppTests.m
+//  Peck AppTests
+//
+//  Created by Aaron Taylor on 3/5/14.
+//  Copyright (c) 2014 Peck App. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Peck_AppTests : XCTestCase
+
+@end
+
+@implementation Peck_AppTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
