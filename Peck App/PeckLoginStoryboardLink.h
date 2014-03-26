dabe78ced82874685b25908e5169b49da7e167f1
kujenga <ataylor0123@gmail.com>
2014-03-25 23:25:18 -0400

    added custom classes for the link from login storyboard to the main storyboard

---
+++ Peck App/PeckLoginStoryboardLink.h
@@ -0,0 +1,13 @@
+//
+//  PeckLoginStoryboardLink.h
+//  PeckDev
+//
+//  Created by Aaron Taylor on 3/23/14.
+//  Copyright (c) 2014 Peck App. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PeckLoginStoryboardLink : UIViewController
+
+@end
